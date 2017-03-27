28363e2ca60a9ed470edbd5f47bbb7433e36fc54
fayostyle <phuang5945@163.com>
2017-03-28 00:41:22 +0800

    Initial commit

---
+++ src/cn/fayostyle/test/Hello.java
@@ -0,0 +1,18 @@
+package cn.fayostyle.test;
+
+/**
+ * Created by HuangPan on 2017/3/28.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("hello world");
+    }
+}
+
+
+
+
+
+
+
+
